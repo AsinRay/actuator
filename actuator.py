1f4354de503d844ac81d25e8e4db7f0c216e3f3a
idoz <dozray@163.com>
2017-12-12 22:56:48 +0800

    init

--- actuator.py
+++ actuator.py
@@ -26,35 +26,43 @@ startSh = acHome + "start.sh"
 checkURL = "http://127.0.0.1:2222/health"
 otcCountURL = "https://otc.forotc.com/robot/getRobotAdClosedCount"
 
-pid = commands.getoutput("cat " + acHome + appName + ".pid")
 
 # 设置超时时间
 socket.setdefaulttimeout(20)
 
+catId = "cat '" + acHome + appName + ".pid" + "'"
+print catId
 
-def reboot_app():
-    if startSh:
-        pid = commands.getoutput("cat " + acHome + appName + ".pid")
-        print (">>> get pid " + pid)
-        reboot_process(pid, startSh)
+
+def get_pid():
+    rtn = commands.getoutput(catId)
+    if rtn.isdigit():
+        return rtn
+    else:
+        return 0
 
 
 def reboot_process(pid, start_sh):
-    if pid:
+    if not start_sh:
+        print("start_sh must not be null")
+        return
+    if get_pid():
         print dtNow, "kill pid " + pid
         os.system("kill " + pid)
         os.system("kill -9 " + pid)
-        time.sleep(3)
-        os.system(start_sh)
-    else:
-        print ("error pid is null.")
+        time.sleep(2)
+    os.system(start_sh)
+
+
+def send_get_request(url):
+    pass
 
 
 def check_robot_alive():
 
     print ("------------- check robot alive ---------------")
     print dtNow, "开始检查 robot..."
-    if pid:
+    if get_pid():
         print dtNow, "进程已存在，检测health url是否正常..."
         try:
             response_code = urllib.urlopen(checkURL).getcode()
@@ -62,11 +70,11 @@ def check_robot_alive():
                 print dtNow, "URL可以正常访问，无需重启robot."
             else:
                 print dtNow, "页面访问出错,开始重启robot."
-                reboot_app()
+                reboot_process(get_pid(), startSh)
         except Exception, e:
             print "type(e)=%s" % type(e)
             print(dtNow, "无法访问到health, 准备重启动robot... ")
-            reboot_app()
+            reboot_process(get_pid(), startSh)
     else:
         pass
         print dtNow, "进程不存在，准备启动robot... "
@@ -79,13 +87,12 @@ def check_closed_ad_per_15_min():
     print("All robot close ad count:" + response)
     if int(response) < 15:
         print("12分钟内取得的数据小于15，正在重启robot服务器...")
-        reboot_app()
+        reboot_process(get_pid(), startSh)
     else:
         print("Status OK... ")
 
 
 while True:
-    time.sleep(10)
+    time.sleep(2)
     check_robot_alive()
-
-    check_closed_ad_per_15_min()
+    #check_closed_ad_per_15_min()
